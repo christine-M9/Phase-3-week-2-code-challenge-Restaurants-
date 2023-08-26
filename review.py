34455d1567de6f904135fcf4e9bfa91f976e1d40
Christine <christinekhekama99@gmail.com>
2023-08-26 18:25:57 +0300

    reviews initialized with customer,restaurant and rating

---
+++ review.py
@@ -0,0 +1,21 @@
+from customer import Customer
+from restaurant import Restaurant
+#iniatializing 
+class Review:
+    def __init__(self, customer, restaurant, rating):
+        self.customer = customer
+        self.restaurant = restaurant
+        self.rating = rating
+
+
+# Creating customer and restaurant
+customer1 = Customer("Mathews", "Juma")
+restaurant1 = Restaurant("Hilton Hotel")
+
+# Creating a review 
+review1 = Review(customer1, restaurant1, 8)
+
+# Accessing the review's attributes
+print("Customer:", review1.customer.given_name())
+print("Restaurant:", review1.restaurant.name())
+print("Rating:", review1.rating)
