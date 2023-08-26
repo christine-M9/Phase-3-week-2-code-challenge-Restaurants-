80e74127194d54d9b0706cd404eb1c1d73709057
Christine <christinekhekama99@gmail.com>
2023-08-26 17:12:57 +0300

    restaurant initialized

---
+++ restaurant.py
@@ -0,0 +1,11 @@
+#restaurant initialised with name as a string.
+class Restaurant:
+    def __init__(self, name):
+        self.name = name
+
+
+
+
+
+restaurant1 = Restaurant("Ceejay Inn")
+print(restaurant1.name) 
