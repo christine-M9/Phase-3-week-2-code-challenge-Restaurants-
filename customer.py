b354922a7da016c0798db2b7db07e0ea64bd430d
Christine <christinekhekama99@gmail.com>
2023-08-26 15:10:52 +0300

    changes given name

--- customer.py
+++ customer.py
@@ -3,7 +3,16 @@ class Customer:
     def __init__(self, name, family_name):
         self.name = name
         self.family_name = family_name
+#returns given name
+    def given_name(self):
+        return self.name
+#changes given name 
+    def change_given_name(self, new_given_name):
+        self.name = new_given_name
 
-#customer1 = Customer("George", "Washington")
-#print(customer1.name)  
-#print(customer1.family_name)
+
+customer1 = Customer("George", "Washington")
+print(customer1.given_name()) 
+
+customer1.change_given_name("Christine")
+print(customer1.given_name())  
