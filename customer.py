9489cde2768a9c4797eb1221c0428026e8223e2c
Christine <christinekhekama99@gmail.com>
2023-08-26 15:00:40 +0300

    customer initialization

---
+++ customer.py
@@ -0,0 +1,9 @@
+#customer initialised with given name and family name.
+class Customer:
+    def __init__(self, name, family_name):
+        self.name = name
+        self.family_name = family_name
+
+#customer1 = Customer("George", "Washington")
+#print(customer1.name)  
+#print(customer1.family_name)
