2148f7e5bdd79224bb906fc098f603ced1a7523e
Christine <christinekhekama99@gmail.com>
2023-08-26 17:53:12 +0300

    name doesnt change after restaurant is created

--- restaurant.py
+++ restaurant.py
@@ -2,14 +2,17 @@
 class Restaurant:
     def __init__(self, name):
         self._name = name
-#returns name        
+#returns name.
     def name(self):
         return self._name
 
-    
-restaurant1 = Restaurant("Ceejay Inn")
-print(restaurant1.name())  
 
+restaurant1 = Restaurant("Ceejay Inn")
+print(restaurant1.name())
+
+# Attempting to change the restaurant name.
+restaurant1._name = "Jay's Inn"
+print(restaurant1.name()) 
 
 
 
