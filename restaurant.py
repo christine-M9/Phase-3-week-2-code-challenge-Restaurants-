487840cad789b3a0c33ff2eae2f725dd6b235286
Christine <christinekhekama99@gmail.com>
2023-08-26 17:42:31 +0300

    returns restaurant name

--- restaurant.py
+++ restaurant.py
@@ -1,11 +1,16 @@
 #restaurant initialised with name as a string.
 class Restaurant:
     def __init__(self, name):
-        self.name = name
+        self._name = name
+#returns name        
+    def name(self):
+        return self._name
 
-
-
-
-
+    
 restaurant1 = Restaurant("Ceejay Inn")
-print(restaurant1.name) 
+print(restaurant1.name())  
+
+
+
+
+
